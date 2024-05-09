b131618ca0444cb7a09eecaf735760741249296d
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2024-05-09 19:15:00 +0300

    One more API service has been integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './services/api.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +10,31 @@ import { ApiService } from './services/api.service';
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'swiper-carousel';
-  photosArray: any[] = []
+  photosArray: any[] = [];
   isLoading: boolean = true;
 
-  constructor(private api:ApiService) {}
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.api.fetchData().subscribe(
-      (data: any[]) => {  
-        this.photosArray = data.map(item => item.urls.regular);
-        console.log('We received a data', this.photosArray);
+    const fetchData$ = this.api.fetchPhotoFromUnsplash();
+    const fetchDataFromPexels$ = this.api.fetchPhotoFromPexels();
+
+    forkJoin([fetchData$, fetchDataFromPexels$]).subscribe(
+      ([data1, data2]) => {
+        this.photosArray = [
+          ...data1.map(
+            (item: { urls: { regular: any }; src: { landscape: any } }) =>
+              item.urls ? item.urls.regular : item.src.landscape
+          ),
+          ...data2.photos.map(
+            (item: { src: { landscape: any } }) => item.src.landscape
+          ),
+        ];
+        console.log('Combined data:', this.photosArray);
         this.isLoading = false;
       },
       (error) => {
