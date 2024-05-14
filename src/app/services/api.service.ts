8211fc037c54a40b53b045d6a597f562802ce53f
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2024-05-14 11:55:18 +0300

    updated GET-request parameters, added typing for method

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { UnsplashPhoto, PexelsPhotoCollection } from '../interfaces/interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +10,23 @@ import { UnsplashPhoto, PexelsPhotoCollection } from '../interfaces/interface';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  fetchPhotoFromUnsplash() {
-    const url = `https://api.unsplash.com/photos/random?client_id=${environment.unsplashApiKey}&orientation=landscape&count=5`;
-    return this.http.get<UnsplashPhoto[]>(url);
+  fetchPhotoFromUnsplash(): Observable<UnsplashPhoto[]> {
+    const url = `https://api.unsplash.com/photos/random`;
+    const params = new HttpParams()
+      .set('client_id', environment.unsplashApiKey)
+      .set('orientation', 'landscape')
+      .set('count', '5');
+
+    return this.http.get<UnsplashPhoto[]>(url, { params });
   }
 
-  fetchPhotoFromPexels() {
-    const url = `https://api.pexels.com/v1/curated?per_page=5`;
-    return this.http.get<PexelsPhotoCollection>(url, { headers: {Authorization: environment.pexelsApiKey} });
+  fetchPhotoFromPexels(): Observable<PexelsPhotoCollection> {
+    const url = `https://api.pexels.com/v1/curated`;
+    const params = new HttpParams().set('per_page', '5');
+
+    return this.http.get<PexelsPhotoCollection>(url, {
+      headers: { Authorization: environment.pexelsApiKey },
+      params,
+    });
   }
 }
